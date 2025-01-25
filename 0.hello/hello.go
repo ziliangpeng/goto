83a04132800661981297e28961d3db825cd9bb99
Ziliang Peng <ziliangdotme@gmail.com>
2025-01-25 00:09:28 -0800

    -

--- 0.hello/hello.go
+++ 0.hello/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var a int = 10
-    var b int = 20
-    sum := a + b
-    fmt.Println("The sum of a and b is:", sum)
+	var a int = 10
+	var b int = 20
+	fmt.Println("a is", a, ", b is", b)
+	sum := a + b
+	fmt.Println("The sum of a and b is:", sum)
 }
-
